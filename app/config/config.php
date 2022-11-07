3859b3fef73d7f382153cd644fe59a4ef890600b
User <User@DESKTOP-R07R23M>
2022-11-07 11:31:21 +0200

    add config

--- app/config/config.php
+++ app/config/config.php
@@ -3,12 +3,12 @@
 //Define your own constants for your site and database
 
 //Database
-define('DB_HOST', '');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'shop');
 
 //App information
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', '');
+define('URLROOT', 'http://localhost/shop');
 define('SITENAME', '');
